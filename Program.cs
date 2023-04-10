8b083fc11ef448a56ca766a4650c96ce5242a500
Kirill Klimentov <norzernorzer@gmail.com>
2023-04-10 15:22:43 +0300

    ver1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*string [] CreateAndFillArray (int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+*/
+void ArrayComparison(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+      if(array1[i].Length <= 3)
+        {
+          array2[count] = array1[i];
+          count++;
+        }
+    }
+}
